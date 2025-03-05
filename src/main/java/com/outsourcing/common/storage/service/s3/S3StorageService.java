84b83eab2a817f1350d4f473430ba3e9c63a92f5
dereck-jun <yjpark0805@naver.com>
2025-03-05 23:03:05 +0900

    Feat: 이미지 다중 업로드 구현
    - 단순 반복문을 활용해서 구현함
    - 다중 파일 업로드 시 중간에 예외 발생 시 예외 처리 후 바로 예외 던짐
    - `StorageService`에 다중 업로드를 위한 추상 메서드 정의함
    
    Related to: #29

--- src/main/java/com/outsourcing/common/storage/service/s3/S3StorageService.java
+++ src/main/java/com/outsourcing/common/storage/service/s3/S3StorageService.java
@@ -1,9 +1,12 @@
 package com.outsourcing.common.storage.service.s3;
 
 import static com.outsourcing.common.exception.ErrorCode.*;
+import static com.outsourcing.common.storage.enums.UploadType.*;
 import static org.springframework.http.MediaType.*;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.outsourcing.common.exception.BaseException;
+import com.outsourcing.common.storage.dto.response.MultipleUploadResponse;
 import com.outsourcing.common.storage.dto.response.UploadResponse;
 import com.outsourcing.common.storage.enums.UploadType;
 import com.outsourcing.common.storage.service.StorageService;
@@ -38,12 +42,12 @@ public class S3StorageService implements StorageService {
 	private String region;
 
 	@Override
-	public UploadResponse upload(MultipartFile multipartFile, String type, CustomUserDetails currentUser) {
-		checkFileType(multipartFile.getContentType());
+	public UploadResponse upload(MultipartFile originalImage, String type, CustomUserDetails currentUser) {
+		checkFileType(originalImage.getContentType());
 
 		// convertedEmail + / + System.currentTimeMillis() + "_" + originalFilename + fileExtension
 		String filename = FileUtils.buildFilename(
-			Objects.requireNonNull(multipartFile.getOriginalFilename()), currentUser.getUsername());
+			Objects.requireNonNull(originalImage.getOriginalFilename()), currentUser.getUsername());
 
 		UserRole currentUserRole = currentUser.getUserInfo().getRole();
 		UploadType uploadType = UploadType.from(type);
@@ -56,16 +60,48 @@ public class S3StorageService implements StorageService {
 		PutObjectRequest request = PutObjectRequest.builder()
 			.bucket(bucket)
 			.key(key)
-			.contentType(multipartFile.getContentType())
+			.contentType(originalImage.getContentType())
 			.build();
 
-		byte[] resizedImageBytes = FileUtils.resize(multipartFile, 300, 300);
+		byte[] resizedImageBytes = FileUtils.resize(originalImage, 300, 300);
 
 		//  리사이즈 후 반환되는 InputStream의 크기와 S3 업로드 시 제공하는 콘텐츠 길이가 일치해야 함
-		s3Client.putObject(request,
+		s3Client.putObject(
+			request,
 			RequestBody.fromInputStream(new ByteArrayInputStream(resizedImageBytes), resizedImageBytes.length));
 		log.info("https://{}.s3.{}.amazonaws.com/{}", bucket, region, key);
 		return UploadResponse.of(key);
+	}
+
+	@Override
+	public MultipleUploadResponse multipleUpload(
+		List<MultipartFile> originalImages,
+		String type,
+		CustomUserDetails currentUser
+	) {
+
+		int counter = 0;
+		UploadType uploadType = UploadType.from(type);
+		if (!uploadType.isAllowedFor(currentUser.getUserInfo().getRole())) {
+			throw new BaseException(UPLOAD_ACCESS_DENIED);
+		}
+		
+		if (uploadType == PROFILES || uploadType == STORES) {
+			throw new BaseException(INVALID_UPLOAD_TYPE);
+		}
+
+		List<String> responses = new ArrayList<>();
+		for (MultipartFile image : originalImages) {
+			try {
+				counter++;
+				UploadResponse upload = upload(image, type, currentUser);
+				responses.add(upload.getUploadUrl());
+			} catch (Exception e) {
+				log.error("{} 번째 업로드 중 예외 발생: {}", counter, e.getLocalizedMessage());
+				throw new BaseException(IMAGE_UPLOAD_IO_EXCEPTION);
+			}
+		}
+		return MultipleUploadResponse.of(responses);
 	}
 
 	private void checkFileType(String contentType) {
