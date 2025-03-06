94419bfea95a149247853188cf64195deff9ffd5
dereck-jun <yjpark0805@naver.com>
2025-03-06 20:18:29 +0900

    Refactor: user 패키지 내 사용하지 않는 필드 및 메서드 제거

--- src/main/java/com/outsourcing/domain/user/repository/CustomerRepository.java
+++ src/main/java/com/outsourcing/domain/user/repository/CustomerRepository.java
@@ -3,7 +3,6 @@ package com.outsourcing.domain.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.outsourcing.domain.user.entity.Customer;
@@ -16,10 +15,6 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 	// 탈퇴 유저 제외 이메일 검색
 	@Query(value = "select c from Customer c where c.email = :email and c.deletedAt is null")
 	Optional<Customer> findByEmailAndDeletedAt(String email);
-
-	@Modifying
-	@Query(value = "update Customer c set c.deletedAt = current_timestamp()")
-	void softDeleteCustomer();
 
 	boolean existsByEmail(String email);
 
--- src/main/java/com/outsourcing/domain/user/repository/OwnerRepository.java
+++ src/main/java/com/outsourcing/domain/user/repository/OwnerRepository.java
@@ -3,7 +3,6 @@ package com.outsourcing.domain.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.outsourcing.domain.user.entity.Owner;
@@ -16,10 +15,6 @@ public interface OwnerRepository extends JpaRepository<Owner, Long> {
 	// 탈퇴 유저 제외 이메일 검색
 	@Query(value = "select o from Owner o where o.email = :email and o.deletedAt is null")
 	Optional<Owner> findByEmailAndDeletedAt(String email);
-
-	@Modifying
-	@Query(value = "update Owner o set o.deletedAt = current_timestamp()")
-	void softDeleteOwner();
 
 	boolean existsByEmail(String email);
 
