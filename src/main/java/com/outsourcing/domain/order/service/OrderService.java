d00529b5415c26a2b591c9f579c042d84baa7941
qwerdfJIN <test19980224@gmail.com>
2025-03-06 17:45:42 +0900

    fix: 자동 정렬된 OrderService import 순서 복원

--- src/main/java/com/outsourcing/domain/order/service/OrderService.java
+++ src/main/java/com/outsourcing/domain/order/service/OrderService.java
@@ -1,16 +1,15 @@
 package com.outsourcing.domain.order.service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.outsourcing.domain.order.dto.OrderItemDto;
 import com.outsourcing.domain.order.dto.OrderResponse;
 import com.outsourcing.domain.order.repository.OrderRepository;
+
 import com.outsourcing.domain.user.entity.User;
 import com.outsourcing.domain.user.repository.CustomerRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +24,6 @@ public class OrderService {
 			.orElseThrow(() -> new IllegalArgumentException("user not found"));
 
 		// 가게 정보
+
 	}
 }
